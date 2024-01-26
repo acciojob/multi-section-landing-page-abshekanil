aacd68f7f98ef5c0792e9569eaa99ca83c51e5e1
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-26 16:54:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let phoneNumber = document.getElementById('number');
+let message = document.getElementById('message');
+let submitBtn = document.getElementById('btn');
+let validationForm = document.getElementById('validation');
+
+submitBtn.addEventListener('click', function(){
+	validationForm.style.display = "block";
+});
